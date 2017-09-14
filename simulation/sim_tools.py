0742d886c6cdd0386b685581baa3d3a4aba311dd
Rafael Lopez <raflopjr@gmail.com>
2017-09-13 17:33:16 -0700

    added return pos

--- simulation/sim_tools.py
+++ simulation/sim_tools.py
@@ -14,6 +14,7 @@ def get_sim_positions_old(alleles, n, length):
     for i in xrange(n):
         position = round(float(sim_position[i])*float(length) )
         pos.append(position)
+    return pos
 
 
 
