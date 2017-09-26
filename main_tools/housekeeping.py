9ccd86088726e4d9066aaa1ea330251b31c2cc60
Rafael Lopez <raflopjr@gmail.com>
2017-09-25 17:03:24 -0700

    Reduced number of args, model_file.csv needs to include SNP file, germline, and nonrandom_discovery

--- main_tools/housekeeping.py
+++ main_tools/housekeeping.py
@@ -1,5 +1,6 @@
 from main_tools import global_vars
 from main_tools.my_random import MY_RANDOM as random
+import sys
 
 
 def prettyPrintDict(dic):
@@ -17,12 +18,13 @@ def process_args(arguments):
     args = {'command':arguments[0],
             'param file':arguments[1],
             'model file':arguments[2],
-            'sim option':arguments[3],
-            'job':arguments[4],
-            'SNP file':arguments[5],
-            'germline':arguments[6],
-            'random discovery':arguments[7],
-            'path':arguments[8]}
+            'job':arguments[3],
+            'path':arguments[4]}
+    model_args = argsFromModelCSV(args['model file'])
+    args['sim option'] = model_args['sim option']
+    args['SNP file'] = model_args['SNP file']
+    args['germline'] = model_args['germline']
+    args['random discovery'] = model_args['random discovery']
     return args
 
 def set_seed(seed_option):
@@ -32,3 +34,39 @@ def set_seed(seed_option):
         random.seed()
     if seed_option > int(0):
         random.seed(seed_option)
+
+def argsFromModelCSV(filename):
+    #Reads arguments from model_file.csv
+    #returns model_args dictionary
+    f=open(filename, 'r')
+    model_args=dict()
+    for line in f:
+        if line.startswith("-macs") or line.startswith("-macs_file"):
+            x = line.strip().split(",")
+            model_args['sim option']= x[0][1:]
+        if line.startswith("-macsswig"):
+            x = line.strip().split(",")
+            model_args['sim option']= x[0][1:]
+        if line.startswith("-array"):
+            x = line.strip().split(",")
+            if x[1].startswith(" "):
+                model_args['SNP file']= x[1][1:]
+            else:
+                model_args['SNP file']= x[1]
+        if line.startswith("-germline"):
+            model_args['germline']= 0
+        if line.startswith("-nonrandom_discovery"):
+            model_args['random discovery'] = False
+        
+    if 'sim option' not in model_args:
+        print("Sim option not provided in model_file.csv")
+        sys.exit(1)
+    if 'germline' not in model_args:
+        model_args['germline'] = 1
+    if 'random discovery' not in model_args:
+        model_args['random discovery'] = True
+    if 'SNP file' not in model_args:
+        print("No SNP file provided in model_file.csv")
+        sys.exit(1)
+    return model_args
+        
