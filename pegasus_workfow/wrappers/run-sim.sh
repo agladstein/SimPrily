7ddade414e8a6ddeb1d8b06289f6030d0839e718
Ariella Gladstein <agladstein@email.arizona.edu>
2017-10-01 13:11:10 -0700

    update run-sim.sh to run with new simprily command with less arguments. indicate in dax-generator what needs to be changed

--- pegasus_workfow/wrappers/run-sim.sh
+++ pegasus_workfow/wrappers/run-sim.sh
@@ -6,19 +6,17 @@ set -v
 JOB_ID=$1
 PARAM_FILE=$2
 MODEL_FILE=$3
-ARRAY_FILE=$4
-MAP_FILE=$5
 
 #export PYTHONPATH=/app
 export PATH=/usr/local/bin:/usr/bin:/bin
 ln -s /app/bin bin
 touch outputs-$JOB_ID.tar.gz
 
-CMD="python /app/simprily.py $PARAM_FILE $MODEL_FILE macs $JOB_ID $ARRAY_FILE 1 True ."
+CMD="python /app/simprily.py $PARAM_FILE $MODEL_FILE $JOB_ID ."
 echo
 echo "Running: $CMD"
 $CMD
 
 # output files need unique names in the top level dir
-tar czf outputs-$JOB_ID.tar.gz results_sims* sim_values*
+tar czf outputs-$JOB_ID.tar.gz results_*
 
