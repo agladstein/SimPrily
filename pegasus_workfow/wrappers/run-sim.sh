3e22de1ae4247da5a9c38e05986397aea16e8ba3
Ariella Gladstein <agladstein@email.arizona.edu>
2017-11-26 12:57:49 -0700

    refactored bash

--- pegasus_workfow/wrappers/run-sim.sh
+++ pegasus_workfow/wrappers/run-sim.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e
-set -v
+set -euo pipefail
 
 JOB_ID=$1
 PARAM_FILE=$2
@@ -10,7 +9,6 @@ MODEL_FILE=$3
 #export PYTHONPATH=/app
 export PATH=/usr/local/bin:/usr/bin:/bin
 ln -s /app/bin bin
-#touch outputs-$JOB_ID.tar.gz
 touch results_${JOB_ID}.txt
 
 CMD="python /app/simprily.py $PARAM_FILE $MODEL_FILE $JOB_ID ."
