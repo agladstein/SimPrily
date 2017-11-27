2cf887a1b2fb6e0d76f656c1eaa3a775d645a4c6
Ariella Gladstein <agladstein@email.arizona.edu>
2017-11-26 20:08:47 -0700

    trying to pass optional arguments to pegasus

--- pegasus_workfow/wrappers/run-sim.sh
+++ pegasus_workfow/wrappers/run-sim.sh
@@ -5,13 +5,28 @@ set -euo pipefail
 JOB_ID=$1
 PARAM_FILE=$2
 MODEL_FILE=$3
+ARRAY_FILE=$4
+REC_MAP=$5
 
 #export PYTHONPATH=/app
 export PATH=/usr/local/bin:/usr/bin:/bin
 ln -s /app/bin bin
 touch results_${JOB_ID}.txt
 
-CMD="python /app/simprily.py $PARAM_FILE $MODEL_FILE $JOB_ID ."
+if [ -n "$REC_MAP" -a -n "$ARRAY_FILE" ]; then
+  echo 1
+  CMD="python /app/simprily.py -p $PARAM_FILE -m $MODEL_FILE -i $JOB_ID -o . -g $REC_MAP -a $ARRAY_FILE"
+elif [ -n "$REC_MAP" -a -z "$ARRAY_FILE" ]; then
+  echo 2
+  CMD="python /app/simprily.py -p $PARAM_FILE -m $MODEL_FILE -i $JOB_ID -o . -g $REC_MAP"
+elif [ -z "$REC_MAP" -a -n "$ARRAY_FILE" ]; then
+  echo 3
+  CMD="python /app/simprily.py -p $PARAM_FILE -m $MODEL_FILE -i $JOB_ID -o . -a $ARRAY_FILE"
+else
+  echo 4
+  CMD="python /app/simprily.py -p $PARAM_FILE -m $MODEL_FILE -i $JOB_ID -o ."
+fi
+
 echo
 echo "Running: $CMD"
 $CMD
