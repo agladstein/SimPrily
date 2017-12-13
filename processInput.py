c50d2938194369b467fa281cd5fd4ee528435e29
David Christy <DavidChristy520@gmail.com>
2017-12-12 18:03:27 -0700

    Working on cleaning up processing input

--- processInput.py
+++ processInput.py
@@ -55,7 +55,13 @@ def read_params_file(in_file):
     return model_params_dict
 
 
-def get_param_value_bounded(temp_num, temp_low=False):
+def get_param_value_bounded(temp_num, temp_low):
+    """
+
+    :param temp_num:
+    :param temp_low:
+    :return:
+    """
 
     temp_num = temp_num.strip()
     if ":" not in temp_num:
@@ -70,23 +76,6 @@ def get_param_value_bounded(temp_num, temp_low=False):
     return return_value
 
 
-def prior_to_param_value(input_param_str):
-    """
-    priorToParamValue:
-    This is a helper function that takes a string in the form (###:###)
-    Numbers are allowed to be either scientific notation or base 10
-    And returns a random value in that range, in the form of a string
-    """
-
-    assert isinstance(input_param_str, str), "priorToParamValue called without a string"
-
-    temp_low = float(sci_to_float(input_param_str.split(":")[0][1:]))
-    temp_high = float(sci_to_float(input_param_str.split(":")[1][:-1]))
-    return_value = str(random.uniform(temp_low, temp_high))
-
-    return return_value
-
-
 # TODO: Confirm that this code doesn't break when you use a range without attaching it to a variable
 def get_param_value_un_bounded(model_params_dict, param_key):
     """
@@ -96,26 +85,36 @@ def get_param_value_un_bounded(model_params_dict, param_key):
     If there is a value that is a given range it will pick a float in that range
     """
 
-    # TODO, add assert for that if later
-    if param_key in model_params_dict.keys():
-        param_raw_value = model_params_dict[param_key]
-        # Strip is here to remove any whitespace that might be in param file
-        # TODO: This strip should be done when we make the dictionary
-        param_raw_value_striped = param_raw_value.strip()
-        if ":" in param_raw_value_striped:
-            # This means you want range
-            unscaled_param_value = prior_to_param_value(param_raw_value_striped)
-        else:
-            unscaled_param_value = str(sci_to_float(param_raw_value_striped))
-        # Updating the Params dict with the chosen value
-        # model_params_dict[param_key] = unscaled_param_value
-        print("paramKey: {}".format(param_key))
+    assert param_key in model_params_dict, "get_param_value_un_bounded called without proper param"
 
+    param_raw_value = model_params_dict[param_key]
+    if ":" in param_raw_value:
+        # This means you want range
+        unscaled_param_value = prior_to_param_value(param_raw_value)
     else:
-        # TODO, this else needs to be removed...if it goes
-        unscaled_param_value = param_key
-        # into this else, this function shouldn't have been called
+        unscaled_param_value = str(sci_to_float(param_raw_value))
+
     return unscaled_param_value
+
+
+def prior_to_param_value(input_param_str):
+    """
+    priorToParamValue:
+    This is a helper function that takes a string in the form (###:###)
+    Numbers are allowed to be either scientific notation or base 10
+    And returns a random value in that range, in the form of a string
+
+    :param input_param_str:
+    :return:
+    """
+
+    assert isinstance(input_param_str, str), "priorToParamValue called without a string"
+
+    temp_low = float(sci_to_float(input_param_str.split(":")[0][1:]))
+    temp_high = float(sci_to_float(input_param_str.split(":")[1][:-1]))
+    return_value = str(random.uniform(temp_low, temp_high))
+
+    return return_value
 
 
 def sci_to_float(s):
@@ -146,7 +145,14 @@ def process_time_data(flag, last_time, model_params_dict_raw, time_data_raw):
     """
     This is a helper function that takes the raw time data from the model
     file and replaces it with the correct value in the params file.
+
+    :param flag:
+    :param last_time:
+    :param model_params_dict_raw:
+    :param time_data_raw:
+    :return:
     """
+
     low_time_used = False
     if "_" in flag and int(flag.split("_")[1]) == 1:
         # There is no time constraint
@@ -161,7 +167,10 @@ def process_time_data(flag, last_time, model_params_dict_raw, time_data_raw):
         if low_time_used:
             time_data = get_param_value_bounded(time_data_raw, last_time)
         else:
-            time_data = get_param_value_un_bounded(model_params_dict_raw, time_data_raw)
+            if time_data_raw in model_params_dict_raw.keys():
+                time_data = get_param_value_un_bounded(model_params_dict_raw, time_data_raw)
+            else:
+                time_data = time_data_raw
 
     return time_data
 
@@ -189,49 +198,6 @@ def populate_flags(model_data_raw):
     return flags
 
 
-def scale_argument(flag, argument_raw, ne):
-    """
-    A helper function that scaled one index in the argument.
-    It returns the scaled number and the index the scaled numbers goes to.
-    To add more flags to this, add it to the matching index group and matching scale group
-    """
-    index = -1
-    # Get the index for the value you are changing
-    index0 = ["-t", "-r", "-G"]
-    if flag in index0:
-        index = 0
-    index1 = ["-eM", "-g", "-eN", "-n"]
-    if flag in index1:
-        index = 1
-
-    index2 = ["-en", "-eg", "-es", "-m", "-em"]
-    if flag in index2:
-        index = 2
-
-    index3 = ["-em"]
-    if flag in index3:
-        index = 3
-
-    assert index != -1, "scale_argument was called with a flag that is not defined in the index lists."
-
-    param = argument_raw[index]
-
-    # scale the number when we have it
-    scale_factor1 = [
-        "-em", "-eM", "-g", "-eg", "-m", "-t", "-r", "-G",
-    ]
-    scale_factor2 = ["-eN", "-n", "-en"]
-
-    if flag in scale_factor1:
-        scaled_param = str(float(4 * (float(param) * ne)))
-    elif flag in scale_factor2:
-        scaled_param = str(float((float(param) / ne)))
-    else:
-        scaled_param = param
-
-    return scaled_param, index
-
-
 def define_non_time_priors(model_params_dict_raw):
     """
 
@@ -244,7 +210,7 @@ def define_non_time_priors(model_params_dict_raw):
     for param_raw_value in model_params_dict_raw:
         # if it has _t in it, ignore for now
         if param_raw_value[-2:].lower() == "_t":
-            model_params_dict[param_raw_value] = model_params_dict_raw[param_raw_value]
+            model_params_dict[param_raw_value[:-2]+"_t"] = model_params_dict_raw[param_raw_value]
             continue
         # TODO: check to make sure value is stripped before here
         prior = model_params_dict_raw[param_raw_value]
@@ -276,34 +242,39 @@ def define_time_priors(model_params_dict_raw, model_data_raw):
     """
     timed_flags = []
     model_params_dict = filter_out_timed_params(model_params_dict_raw)
-    for i, argument in enumerate(model_data_raw):
+    for argument in filter(lambda x: x.startswith("-e"), model_data_raw):
         arg_split = argument.split(',')
         flag = arg_split[0]
-        # ignoring all other flags for now
-        if not flag.startswith("-e"):
-            continue
 
-        # TODO: Confirm that the line is stripped before this
         time_data_raw = arg_split[1]
-        if timed_flags:
-            last_time_value = timed_flags[-1].split(',')[1]
-            time_data = process_time_data(flag, last_time_value, model_params_dict_raw, time_data_raw)
-        else:
-            time_data = get_param_value_un_bounded(model_params_dict_raw, time_data_raw)
+        time_data = get_time_data(flag, model_params_dict_raw, time_data_raw, timed_flags)
 
         # find time variable
-        tmp = ""
-        for tmp in arg_split:
-            if tmp.lower().endswith("_t"):
+        tmp_var = ""
+        for tmp_var in arg_split:
+            if tmp_var.lower().endswith("_t"):
                 break
-        if tmp in model_params_dict_raw and tmp.endswith("_t"):
-            model_params_dict[tmp] = time_data
-        tmp = [argument.split(",")[0],time_data]
-        tmp.extend(argument.split(",")[2:])
-        argument = ",".join(tmp)
+        if tmp_var in model_params_dict_raw and tmp_var.endswith("_t"):
+            model_params_dict[tmp_var] = time_data
+
+        argument_list = [argument.split(",")[0], time_data]
+        argument_list.extend(argument.split(",")[2:])
+        argument = ",".join(argument_list)
         timed_flags.append(argument)
 
     return model_params_dict
+
+
+def get_time_data(flag, model_params_dict_raw, time_data_raw, timed_flags):
+    if timed_flags:
+        last_time_value = timed_flags[-1].split(',')[1]
+        time_data = process_time_data(flag, last_time_value, model_params_dict_raw, time_data_raw)
+    else:
+        if time_data_raw in model_params_dict_raw.keys():
+            time_data = get_param_value_un_bounded(model_params_dict_raw, time_data_raw)
+        else:
+            time_data = time_data_raw
+    return time_data
 
 
 def define_priors(model_params_dict_raw, model_data_raw):
@@ -332,9 +303,7 @@ def substitute_variables(model_params_variables, model_data_raw):
     for argument_raw in model_data_raw:
         argument_split_raw = argument_raw.split(",")
         argument_split = []
-        for i, parameter_raw in enumerate(argument_split_raw):
-            if not parameter_raw:
-                continue
+        for parameter_raw in filter(lambda x: x != "", argument_split_raw):
             parameter = parameter_raw.strip()
             if parameter in model_params_variables:
                 argument_split.append(model_params_variables[parameter])
@@ -349,6 +318,7 @@ def substitute_variables(model_params_variables, model_data_raw):
                     argument_split.append(parameter)
         model_data_list.append(argument_split)
 
+    # generate clean model data
     model_data = []
     for argument in model_data_list:
         model_data.append(",".join(argument))
@@ -413,7 +383,7 @@ def remove_ignored_flags(flags_bloated):
     :param flags_bloated:
     :return:
     """
-    assert isinstance(flags_bloated,OrderedDict), "Given flag value was not a OrderedDict"
+    assert isinstance(flags_bloated, OrderedDict), "Given flag value was not a OrderedDict"
 
     flags = OrderedDict()
     ignored_flags = ["-germline",
@@ -432,24 +402,37 @@ def process_type1_flags(flags):
     # TODO Don't loop through this, instead pull out type1, ignoring the rest
 
     processed_data = {}
-    for flag in flags.keys():
-        # Looping through every key
-        debugPrint(3, "FLAG:  {}: {}".format(flag, flags[flag]))
-        for argument_raw in flags[flag]:
-            if flag == "-discovery":
-                processed_data['discovery'] = [int(s.strip()) for s in argument_raw if s]
-            if flag == "-sample":
-                processed_data['sample'] = [int(s.strip()) for s in argument_raw if s]
-            if flag == "-daf":
-                processed_data['daf'] = float(argument_raw[0])
-            if flag == "-length":
-                processed_data['length'] = argument_raw[0]
-            if flag == "-macs":
-                processed_data['macs'] = argument_raw[0]
-            if flag == "-I":
-                processed_data["I"] = [int(s.strip()) for s in argument_raw[1:] if s]
-            if flag == "-macsswig":
-                processed_data['macsswig'] = argument_raw[0]
+
+    discovery = flags.get("-discovery", None)
+    if discovery:
+        for argument_raw in flags['-discovery']:
+            processed_data['discovery'] = [int(s.strip()) for s in argument_raw if s]
+
+    sample = flags.get("-sample", None)
+    if sample:
+        for argument_raw in flags['-sample']:
+            processed_data['sample'] = [int(s.strip()) for s in argument_raw if s]
+
+    i = flags.get("-I", None)
+    if i:
+        for argument_raw in flags['-I']:
+            processed_data['I'] = [int(s.strip()) for s in argument_raw if s]
+
+    daf = flags.get("-daf", None)
+    if daf:
+        processed_data['daf'] = float(daf[0][0])
+
+    length = flags.get("-length", None)
+    if length:
+        processed_data['length'] = float(length[0][0])
+
+    macs = flags.get("-macs", None)
+    if macs:
+        processed_data['macs'] = float(macs[0][0])
+
+    macsswig = flags.get("-macsswig", None)
+    if macsswig:
+        processed_data['macsswig'] = float(macsswig[0][0])
 
     return processed_data
 
@@ -470,6 +453,49 @@ def filter_out_type1(flags_raw):
     return flags
 
 
+def scale_argument(flag, argument_raw, ne):
+    """
+    A helper function that scaled one index in the argument.
+    It returns the scaled number and the index the scaled numbers goes to.
+    To add more flags to this, add it to the matching index group and matching scale group
+    """
+    index = -1
+    # Get the index for the value you are changing
+    index0 = ["-t", "-r", "-G"]
+    if flag in index0:
+        index = 0
+    index1 = ["-eM", "-g", "-eN", "-n"]
+    if flag in index1:
+        index = 1
+
+    index2 = ["-en", "-eg", "-es", "-m", "-em"]
+    if flag in index2:
+        index = 2
+
+    index3 = ["-em"]
+    if flag in index3:
+        index = 3
+
+    assert index != -1, "scale_argument was called with a flag that is not defined in the index lists."
+
+    param = argument_raw[index]
+
+    # scale the number when we have it
+    scale_factor1 = [
+        "-em", "-eM", "-g", "-eg", "-m", "-t", "-r", "-G",
+    ]
+    scale_factor2 = ["-eN", "-n", "-en"]
+
+    if flag in scale_factor1:
+        scaled_param = str(float(4 * (float(param) * ne)))
+    elif flag in scale_factor2:
+        scaled_param = str(float((float(param) / ne)))
+    else:
+        scaled_param = param
+
+    return scaled_param, index
+
+
 # TODO: Make this code simpler
 def scale_flags(flags_raw):
 
@@ -483,55 +509,62 @@ def scale_flags(flags_raw):
         debugPrint(3, "FLAG:  {}: {}".format(flag, flags_raw[flag]))
         arguments = []
         for argument_raw in flags_raw[flag]:
-            type2_flags = [
-                "-em", "-eM", "-g", "-eN", "-n", "-en",
-                "-eg", "-es", "-m", "-t", "-r", "-G"
-            ]
 
-            argument = []
-            if flag.startswith('-e'):
-                # all <t>'s are scaled
-                scaled_time = str(round(float(argument_raw[0])) / (4 * ne))
-                argument.append(scaled_time)
+            argument = generate_argument(argument_raw, flag, ne)
 
-            if flag in type2_flags:
-                scaled_param, index = scale_argument(flag, argument_raw, ne)
-                for i, param in enumerate(argument_raw):
-                    if i != index:
-                        if flag.startswith('-e') and i == 0:
-                            # TODO: figure out how to "not" this correctly
-                            pass
-                        else:
-                            argument.append(param)
-
-                    else:
-                        argument.append(scaled_param)
-
-            # These scales are difference since they scale all possible values in list
-            # TODO: I really don't think ema and ma flags work, get a test to test them
-            if flag == "-ema":
-                scaled_params = argument_raw[:1]
-                for i in range(2, len(argument_raw)):
-                    param = argument_raw[i]
-                    scaled_params.append(str(float(4 * (float(param) * ne))))
-
-            elif flag == "-ma":
-                scaled_params = argument_raw[0]
-                for i in range(len(argument_raw)):
-                    param = argument_raw[i]
-                    scaled_params.append(str(float(4 * (float(param) * ne))))
-
-            if not argument:
-                for param in argument_raw:
-                    argument.append(param)
-
-            if len(argument) == 1 and flag.startswith("-e"):
-                for param in argument_raw[1:]:
-                    argument.append(param)
             arguments.append(argument)
         flags[flag] = arguments
 
     return flags
+
+
+def generate_argument(argument_raw, flag, ne):
+    type2_flags = [
+        "-em", "-eM", "-g", "-eN", "-n", "-en",
+        "-eg", "-es", "-m", "-t", "-r", "-G"
+    ]
+    argument = []
+    if flag.startswith('-e'):
+        # all <t>'s are scaled
+        scaled_time = str(round(float(argument_raw[0])) / (4 * ne))
+        argument.append(scaled_time)
+
+    if flag in type2_flags:
+        scaled_param, index = scale_argument(flag, argument_raw, ne)
+        append_argument(argument, argument_raw, flag, index, scaled_param)
+
+    # These scales are difference since they scale all possible values in list
+    # TODO: I really don't think ema and ma flags work, get a test to test them
+    if flag == "-ema":
+        argument = argument_raw[:1]
+        for param in argument_raw[2:]:
+            argument.append(str(float(4 * (float(param) * ne))))
+
+    elif flag == "-ma":
+        argument = argument_raw[0]
+        for param in argument_raw:
+            argument.append(str(float(4 * (float(param) * ne))))
+
+    if not argument:
+        for param in argument_raw:
+            argument.append(param)
+
+    if len(argument) == 1 and flag.startswith("-e"):
+        for param in argument_raw[1:]:
+            argument.append(param)
+    return argument
+
+
+def append_argument(argument, argument_raw, flag, index, scaled_param):
+    for i, param in enumerate(argument_raw):
+        if i != index:
+            if flag.startswith('-e') and i == 0:
+                # TODO: figure out how to "not" this correctly
+                pass
+            else:
+                argument.append(param)
+        else:
+            argument.append(scaled_param)
 
 
 def add_events_to_seasons(flags):
@@ -571,6 +604,13 @@ def populate_macs_args(macs_args, scaled_flags):
 
 
 def process_input_files(param_file, model_file, args):
+    """
+
+    :param param_file:
+    :param model_file:
+    :param args:
+    :return:
+    """
     """
     This is the function that takes links to two files and outputs a dictionary (processedData)
     With all the (useful) data in the two files
