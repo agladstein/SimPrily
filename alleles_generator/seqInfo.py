b6328d896a725bae0ed9f87b61d05a956e45cc0b
David Christy <DavidChristy520@gmail.com>
2018-02-19 16:00:26 -0700

    fixed the -I issue

--- alleles_generator/seqInfo.py
+++ alleles_generator/seqInfo.py
@@ -7,62 +7,61 @@ from main_tools.housekeeping import debugPrint
 class SeqInfo():
     def __init__(self, name, total, seq_type):
         self.name = name
-        self.tot  = total
-        self.type = seq_type   # Either 'discovery' or 'sample'
+        self.tot = total
+        self.type = seq_type  # Either 'discovery' or 'sample'
         self.bits = bitarray()
         self.asc_bits = bitarray()
 
         ## Discovery-specific variables:
-        self.panel    = 0   # panel is the number of discovery individuals used to make the pseudoarray, but which do not appear in the pseudoarray
-        self.genotyped   = 0   # genotyped is the number of discovery individuals in the pseudoarray not used in the panel
+        self.panel = 0  # panel is the number of discovery individuals used to make the pseudoarray, but which do not appear in the pseudoarray
+        self.genotyped = 0  # genotyped is the number of discovery individuals in the pseudoarray not used in the panel
         self.CGI_bits = bitarray() if seq_type == 'discovery' else None
-        self.pi_CGI   = []         if seq_type == 'discovery' else None
-        self.pi_asc   = None
+        self.pi_CGI = [] if seq_type == 'discovery' else None
+        self.pi_asc = None
 
     def __repr__(self):
         return self.name
 
-def create_sequences(processedData, args):
 
+def create_sequences(processedData, args):
     '''
     Parameters: args is a dictionary that maps the SNP file to 
     array_template
     args: a dictionary (seen below in the args parameter)
     processedData: a dictionary (seen below in the args parameter)
-    
-    Returns: instance types named [d1, s1] 
+
+    Returns: instance types named [d1, s1]
     '''
-    
-    debugPrint(2,"Running create_sequences:")
+
+    debugPrint(2, "Running create_sequences:")
     sequences = []
     if 'discovery' in processedData and 'sample' in processedData and 'daf' in processedData:
-    ### Initialize all discovery type sequence data
+        ### Initialize all discovery type sequence data
         for i, ind in enumerate(processedData.get('discovery')):
             tot_index = processedData['macs_args'].index("-I") + 1 + ind
-            tot = int(processedData['macs_args'][tot_index]) # total number of individuals used in simulation
+            tot = int(processedData['macs_args'][tot_index])  # total number of individuals used in simulation
             name = processedData.get('name').pop(0)
-            seq = SeqInfo(name, tot, seq_type = 'discovery')
+            seq = SeqInfo(name, tot, seq_type='discovery')
 
-            seq.genotyped = processedData['I'][ind - 1]
-            seq.panel  = seq.tot - seq.genotyped
+            seq.genotyped = processedData['I'][ind]
+            seq.panel = seq.tot - seq.genotyped
             sequences.append(seq)
 
         ### Initialize all sample type sequence data
         for i, ind in enumerate(processedData.get('sample')):
-            tot = processedData['I'][ind-1]
+            tot = processedData['I'][ind]
             name = processedData.get('name').pop(0)
-            seq = SeqInfo(name, tot, seq_type = 'sample')
+            seq = SeqInfo(name, tot, seq_type='sample')
 
             seq.panel = seq.tot
             seq.genotyped = seq.tot
             sequences.append(seq)
     else:
-        for ind in range(int(processedData['macs_args'][4])):
-            tot = processedData['I'][ind-1]
+        for tot in processedData["I"][1:]:
             name = processedData.get('name').pop(0)
-            seq = SeqInfo(name, tot, seq_type = 'discovery')
+            seq = SeqInfo(name, tot, seq_type='discovery')
 
-          #  seq.panel = seq.tot #pretty sure it can be deleted
+            #  seq.panel = seq.tot #pretty sure it can be deleted
             seq.genotyped = seq.tot
             sequences.append(seq)
     return sequences
--- autoTesting.sh
+++ autoTesting.sh
@@ -1,29 +1,44 @@
 #!/usr/bin/env bash
+
+
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+
  total=0
  bad=0
- i=3
- while true;
- do
- 	total=$((total+1))
- 	~/simprily_env/bin/python simprily.py -p examples/eg${i}/param_file_eg${i}.txt -m examples/eg${i}/model_file_eg${i}.csv -g genetic_map_b37/genetic_map_GRCh37_chr1.txt.macshs -a array_template/ill_650_test.bed -i ${i} -o output_dir
- 	result=$?
- 	if [[ $result != 0 ]]; then
- 		bad=$((bad+1))
+i=6
+#if [ $1 != "a" ]; then echo hi; fi
+#while true;
+#do
+#    total=$((total+1))
+# 	~/simprily_env/bin/python simprily.py -p examples/eg${i}/param_file_eg${i}_asc.txt -m examples/eg${i}/model_file_eg${i}_asc.csv -g genetic_map_b37/genetic_map_GRCh37_chr1.txt.macshs -a array_template/ill_650_test.bed -i ${i} -o output_dir
+# 	result=$?
+# 	if [[ $result != 0 ]]; then
+# 		bad=$((bad+1))
+#
+# 	else
+# 		echo "example $i has passed"
+# 	fi
+# 	percent=$(awk "BEGIN { pc=100-(100*${bad}/${total}); i=int(pc); print (pc-i<0.5)?i:i+1 }")
+# 	echo $percent
+# done
 
- 	else
- 		echo "example $i has passed"
- 	fi
- 	percent=$(awk "BEGIN { pc=100-(100*${bad}/${total}); i=int(pc); print (pc-i<0.5)?i:i+1 }")
- 	echo $percent
- done
-
-for i in 6 1 3 4 7; 
+declare -a arr=("" "_asc")
+for i in 4 7 6 1 3;
 do
-	~/simprily_env/bin/python simprily.py -p examples/eg${i}/param_file_eg${i}.txt -m examples/eg${i}/model_file_eg${i}.csv -g genetic_map_b37/genetic_map_GRCh37_chr1.txt.macshs -a array_template/ill_650_test.bed -i ${i} -o output_dir > /dev/null
-	result=$?
-	if [[ $result != 0 ]]; then
-		echo "example $i has failed" 
-		exit 1
-	fi
-	echo "example $i has passed"
+    for file_type in "${arr[@]}"
+    do
+        ~/simprily_env/bin/python simprily.py -p examples/eg${i}/param_file_eg${i}${file_type}.txt -m examples/eg${i}/model_file_eg${i}${file_type}.csv -g genetic_map_b37/genetic_map_GRCh37_chr1.txt.macshs -a array_template/ill_650_test.bed -i ${i} -o output_dir > /dev/null
+        result=$?
+        if [[ $result != 0 ]]; then
+            echo "example $i$file_type has failed"
+            exit 1
+        fi
+        echo "example $i$file_type has passed"
+    done
 done
